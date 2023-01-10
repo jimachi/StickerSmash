d9d7c2743d3aafeafa373615e70570b3cfbe2570
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-11 01:44:16 +0900

    feat: ステッカーをダブルタップするとサイズが２倍になる

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 import IconButton from './components/IconButton';
@@ -46,7 +47,7 @@ export default function App() {
   const onSaveImageAsync = async () => {};
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={placeholderImage}
@@ -85,7 +86,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
